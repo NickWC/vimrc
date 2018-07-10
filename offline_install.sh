fb56d120c3568105a85016dc32f43aee1f4efb85
Nimo Hsieh <nimo1491@gmail.com>
2018-07-10 13:11:09 +0800

    Add ycm_extra_conf

--- offline_install.sh
+++ offline_install.sh
@@ -2,10 +2,12 @@
 
 # No more support Windows
 VIMRC=".vimrc"
+YCMCONF=".ycm_extra_conf.py"
 VIMPF=".vim"
 
-# vimrc
+# vimrc and ycm conf
 ln -sf $VIMPF/vimrc $VIMRC
+ln -sf $VIMPF/ycm_extra_conf.py $YCMCONF
 
 # finish
 echo "done"
