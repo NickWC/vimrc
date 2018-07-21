5d15eb53990274626663e831e9a3e294b3a59963
Nimo Hsieh <nimo1491@gmail.com>
2018-07-21 22:15:25 +0800

    Update offline install script

--- offline_install.sh
+++ offline_install.sh
@@ -1,13 +1,15 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 # No more support Windows
-VIMRC=".vimrc"
-YCMCONF=".ycm_extra_conf.py"
-VIMPF=".vim"
+declare -r VIMRC=".vimrc"
+declare -r YCMCONF=".ycm_extra_conf.py"
+
+cd "$(dirname "${BASH_SOURCE[0]}")"
+vim_base="$(pwd)"
 
 # vimrc and ycm conf
-ln -sf $VIMPF/vimrc $VIMRC
-ln -sf $VIMPF/ycm_extra_conf.py $YCMCONF
+ln -sf $vim_base/vimrc $HOME/$VIMRC
+ln -sf $vim_base/ycm_extra_conf.py $HOME/$YCMCONF
 
 # finish
 echo "done"
